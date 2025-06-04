6fec748fdf1a8f0b392317e6ad6643c838596ffa
msoaresrj <mvsoaresrj21@gmail.com>
2025-06-03 23:39:23 -0300

    Adicionado Animação na pagina com CSS e JS

--- static/script.js
+++ static/script.js
@@ -22,3 +22,24 @@ navItem.forEach((item) => {
     }
   });
 });
+
+// animar todos os itens na tela que tiverem o meu atributo data-anime
+const item = document.querySelectorAll("[data-anime]");
+
+const animeScroll = () => {
+  const windowTop = window.pageYOffset + window.innerHeight * 0.75;
+  
+  item.forEach((element) => {
+    if (windowTop > element.offsetTop) {
+      element.classList.add("animate");
+    } else {
+      element.classList.remove("animate");
+    }
+  });
+};
+
+animeScroll();
+
+window.addEventListener("scroll", () => {
+  animeScroll();
+});
